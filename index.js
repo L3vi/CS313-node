19567b3d40f424367e2b6356457eec1784c82081
L3vi <levistum@gmail.com>
2018-12-12 22:37:55 -0700

    "Updates..."

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const path = require('path');
-const parse = require('body-parser');
+const bodyParser = require('body-parser');
 
 // Controllers
 const activities = require('./controllers/activities-controller.js');
@@ -15,6 +15,8 @@ const accounts = require('./controllers/accounts-controller.js');
 const clockit = require('./controllers/clockIt-controller.js');
 
 app.use(express.static(path.join(__dirname, '/public/')))
+	.use(bodyParser.json())
+	.use(bodyParser.urlencoded({ extended: true }))
 	// .set('views', path.join(__dirname, 'views'))
 	.get('/', (request, response) => {
 		// response.render('home');
